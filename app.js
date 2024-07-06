197b2d1d43e650262d73787c3e87e46030c5bdf5
romankhan03 <r.k03052029504@gmail.com>
2024-07-07 01:41:03 +0500

    js chap 1 to 3 assignment

---
+++ app.js
@@ -0,0 +1,64 @@
+// chapter 1
+
+alert("Error! please enter a valid password" )
+
+alert("Welcome to JS land..\nHappy coding")
+
+// chapter 2
+// var username = decleration
+var myName = "Roman khan"
+var message = "Hello World"
+alert(message)
+
+var name = "Roman Khan"
+alert(name)
+var age = "19 years old"
+alert(age)
+var occupation = "web development"
+alert(occupation)
+
+var food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(food);
+
+var email = "My email address is example" + "@example.com"
+alert(email)
+
+// chapter 3
+ var age = "i am 19 years old"
+ alert(age)
+
+ 
+
+//  if else statement q1
+// var num = 9 
+//  if(num / 3){
+//     console.log("Number is divisible by 3")
+//  }else{
+//     console.log('Number is not divisible by 3')
+//  }
+// // q2
+//  var num = 4
+//  if(num % 2 === 0){
+//     console.log('the number is even')
+//  }else{
+//     console.log('the numberis odd')
+//  }
+
+
+//  //q3
+
+//  var age = 20
+// if(age > 18){
+//     console.log("Old enough")
+// }else{
+//     console.log("Too young")
+// }
+
+// //q4
+
+
+//  var user_name = prompt("enter your name")
+//  var myname = "Roman khan"
+//  if(user_name === myname){
+//     console.log("hello")
+//  }
